dd4fc49fb489d2fe9fbf391c20fc4d36ed1a2ade
GuillaumeCaz <guillaume.cazeaudumec@gmail.com>
2023-03-12 22:17:36 +0100

    Démarrage du middleware

--- backend/app.js
+++ backend/app.js
@@ -5,9 +5,25 @@ const port = 4200;
 const User = require('./models/userModels');
 const userRoutes = require('./routes/userRoutes');
 const userControllers = require('./controllers/userController')
+const sauceRoutes = require('./routes/sauceRoutes')
+const auth = require('./middleware/auth')
 const cors = require('cors'); 
 const bodyParser = require('body-parser');
+const multer = require('multer');
 require('dotenv').config();
+
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
+});
+
+const upload = multer({ storage: storage });
+
 
 app.use(cors()); 
 
@@ -26,6 +42,7 @@ connectDB()
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/api/auth', userRoutes);
-
+app.use('/api/sauces', auth, sauceRoutes);
+app.use('/uploads', express.static('uploads'));
 
 module.exports = app; 
